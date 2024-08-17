6c8fbc4fb3d9af8bdd8297cea9190d89c948c8f6
NiX <neplaz.dimitri@gmail.com>
2024-08-17 17:02:03 +0200

    fix ui component

--- app/components/ui/typographie.tsx
+++ app/components/ui/typographie.tsx
@@ -6,9 +6,9 @@ interface H2Props {
 
 export function H1({ children }: H2Props) {
     return (
-        <h2 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
             {children}
-        </h2>
+        </h1>
     )
 }
 
@@ -17,5 +17,29 @@ export function H2({ children }: H2Props) {
         <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
             {children}
         </h2>
+    )
+}
+
+export function H3({ children }: H2Props) {
+    return (
+        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+            {children}
+        </h3>
+    )
+}
+
+export function H4({ children }: H2Props) {
+    return (
+        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+            {children}
+        </h4>
+    )
+}
+
+export function P({ children }: H2Props) {
+    return (
+        <p className="leading-7 [&:not(:first-child)]:mt-6">
+            {children}
+        </p>
     )
 }
